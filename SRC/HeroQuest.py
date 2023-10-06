0f644a43ce1fab7fb1b2f083bf8ee5355278ecde
HY_ElCositas <61937572+VanimacionYT@users.noreply.github.com>
2023-10-07 00:22:24 +0200

    Fixed automatic close
    Fixed the bug that caused the game to close after every game adding a final menu.

--- SRC/HeroQuest.py
+++ SRC/HeroQuest.py
@@ -6,6 +6,7 @@ import locale
 import time
 from IPython.display import clear_output
 from datetime import datetime
+gamestatus = 0
 
 #Creamnos la clase Personaje
 class Personaje:
@@ -241,162 +242,185 @@ def LogPartida(Personaje1,Personaje1Name, Personaje2, Personaje2Name,opcion, Tur
     ArchivoLogPA = open('Log/LogPartidas.txt', 'a')
     ArchivoLogPA.write(LogPa)
     ArchivoLogPA.close()
-    
-#Titulo
-print("--------------------")
-print("----[HERO QUEST]----")
-print("--------------------\t\t\t\tV2.6.0\n")
 
-#Sistema para iniciar
-print("Presiona 'ENTER' para empezar!")
-input()
 
-#Borrar lo anterior
-clear_output()
 
-#Se crea un menu para elegir si jugar o borrar logs
-menu = True
-while menu == True:
-    print("<---<-MAIN MENU->--->\n\nJugar HQ: 1\t|\tBorrar Logs:2\n")
-    opcion = input()
-    #Tras seleccionar se limpia la pantalla
-    clear_output()
+while True:
+    if gamestatus == 0:
+        #Titulo
+        print("--------------------")
+        print("----[HERO QUEST]----")
+        print("--------------------\t\t\t\tV2.6.0\n")
 
-#Si se elige borrar logs se aplica un texto de confirmación.
-    if opcion == "2":
-        print("¿Estas seguro de querer borrar los logs?\nSi, Estoy seguro: 1\t|\tNo, No quiero borrarlos: 2")
-        borrar = input()
-        if borrar == "1":
-            #Si elige borarlos se reescriben los logs con un mensaje que indica la fecha de la limpieza y se limpia la consola.
-            clear_output()
-            locale.setlocale(locale.LC_ALL, 'C')
-            dt = datetime.now()
-            LogRefreshDate =  (dt.strftime("[-- Refresh Created At: %d/%m/%Y - %H:%M:%S --]"))
-            ArchivoLogPE = open('Log/LogPersonajes.txt', 'w').close
-            ArchivoLogPE = open('Log/LogPersonajes.txt', 'w')
-            ArchivoLogPE.write("{}\nBienvenido al archivo de Log Personajes\n---------------------------------------".format(LogRefreshDate))
-            ArchivoLogPE.close()
-            ArchivoLogPA = open('Log/LogPartidas.txt', 'w').close
-            ArchivoLogPA = open('Log/LogPartidas.txt', 'w')
-            ArchivoLogPA.write("{}\nBienvenido al archivo de Log Partidas\n-------------------------------------".format(LogRefreshDate))
-            ArchivoLogPA.close()
-            print("¡Logs Reiniciados!")
-        elif borrar == "2":
-            clear_output()
-            menu = True
-    elif opcion == "1":
-        menu = False
+        #Sistema para iniciar
+        print("Presiona 'ENTER' para empezar!")
+        input()
 
-#Mediante un if se selecciona la opcion de parametroa aleatorios o manuales
-print("Quieres colocar parametros de personaje aleatorios o manuales?\n\nAleatorio: 1\t|\tManuales:2\n")
-opcion = input()
+        #Borrar lo anterior
+        clear_output()
+        gamestatus = 1
 
-#Tras seleccionar se limpia la pantalla
-clear_output()
+    elif gamestatus == 1:
+        #Se crea un menu para elegir si jugar o borrar logs
+        menu = True
+        while menu == True:
+            print("<---<-MAIN MENU->--->\n\nJugar HQ: 1\t|\tBorrar Logs:2\n")
+            mainmenu = input()
 
-#Si es aletorio se hace uso del objeto Dado() con sus respectivos metodos
-if opcion == "1":
-    print("\tVamos a definir el nombre de la MOMIA") 
-    print("Nombre Momia")
-    NomM = input()
-    VidM = Dado.tirav()
-    AtaM = Dado.tiraa()
-    DefM = Dado.tirad()
-    Momia1 = Momia(NomM, VidM, AtaM, DefM)
-    
-    print("\n\tVamos a definir el nombre del BARBARO")  
-    print("Nombre Barbaro")
-    NomB = input()
-    VidB = Dado.tirav()
-    AtaB = Dado.tiraa()
-    DefB = Dado.tirad()
-    Barbaro1 = Barbaro(NomB, VidB, AtaB, DefB)
-#Si es manual, el usuario por medio de imputs() selecciona los datos
-elif opcion == "2":    
-    print("\tVamos a definir los valores de la MOMIA") 
-    print("Nombre Momia")
-    NomM = input()
-    print("Vida Momia")
-    VidM = int(input())
-    print("Ataque Momia")
-    AtaM = int(input())
-    print("Defensa Momia")
-    DefM = int(input())
-    Momia1 = Momia(NomM, VidM, AtaM, DefM)
+            #Si se elige borrar logs se aplica un texto de confirmación.
+            if mainmenu == "2":
+                clear_output()
+                print("¿Estas seguro de querer borrar los logs?\nSi, Estoy seguro: 1\t|\tNo, No quiero borrarlos: 2")
+                confirmar = input()
+                
+                if confirmar == "1":
+                    #Si elige borarlos se reescriben los logs con un mensaje que indica la fecha de la limpieza y se limpia la consola.
+                    clear_output()
+                    locale.setlocale(locale.LC_ALL, 'C')
+                    dt = datetime.now()
+                    LogRefreshDate =  (dt.strftime("[-- Refresh Created At: %d/%m/%Y - %H:%M:%S --]"))
+                    ArchivoLogPE = open('Log/LogPersonajes.txt', 'w').close
+                    ArchivoLogPE = open('Log/LogPersonajes.txt', 'w')
+                    ArchivoLogPE.write("{}\nBienvenido al archivo de Log Personajes\n---------------------------------------".format(LogRefreshDate))
+                    ArchivoLogPE.close()
+                    ArchivoLogPA = open('Log/LogPartidas.txt', 'w').close
+                    ArchivoLogPA = open('Log/LogPartidas.txt', 'w')
+                    ArchivoLogPA.write("{}\nBienvenido al archivo de Log Partidas\n-------------------------------------".format(LogRefreshDate))
+                    ArchivoLogPA.close()
+                    print("¡Logs Reiniciados!")
+                elif confirmar == "2":
+                    clear_output()
+                    menu = True
+            elif mainmenu == "1":
+                menu = False
+                gamestatus = 2
 
-    print("\n\tVamos a definir los valores del BARBARO")  
-    print("Nombre Barbaro")
-    NomB = input()
-    print("Vida Barbaro")
-    VidB = int(input())
-    print("Ataque Barbaro")
-    AtaB = int(input())
-    print("Defensa Barbaro")
-    DefB = int(input())
-    Barbaro1 = Barbaro(NomB, VidB, AtaB, DefB)
-else:
-    print("¡ERROR!\nSelecciona una opcion valida")
-    
-#Se borran los textos
-clear_output()
+    elif gamestatus == 2:
 
-#Se muestran los parametros de cada personaje y las opciones de la partida y se guardan en los logs
-if opcion == "1":
-    print("Módo de elección de parámetros: Automático")
-elif opcion == "2":
-    print("Módo de elección de parámetros: Manual")
-print("\n", Momia1)
-print("\n", Barbaro1)
-LogPersonajes(Momia1.nombre, Momia1.logInfo(), Barbaro1.nombre, Barbaro1.logInfo())
+        #Mediante un if se selecciona la opcion de parametroa aleatorios o manuales
+        print("Quieres colocar parametros de personaje aleatorios o manuales?\n\nAleatorio: 1\t|\tManuales:2\n")
+        opcion = input()
 
-#Se indican los turnos
-print("\n¿Cuántos turnos máximos quiere jugar?: ")
-turnos = int(input())
-#Se asigna los valores para el sistema de turnos. J1 es la momia y J2 es el barbaro.
-Atacante = "Bárbaro"
-Defensor = "Momia"
-turno = 0
-for x in range(turnos):
-    turno += 1
-    #Mediante un bucle y varios If deternminamos los turnos
-    time.sleep(1) #implentamos un timesleep(x) para poder hacer que los bucles tengan pausa de x segundos.
-    if Atacante == "Bárbaro":
-        Atacante = "Momia"
-    elif Atacante == "Momia":
+        #Tras seleccionar se limpia la pantalla
+        clear_output()
+
+        #Si es aletorio se hace uso del objeto Dado() con sus respectivos metodos
+        if opcion == "1":
+            print("\tVamos a definir el nombre de la MOMIA") 
+            print("Nombre Momia")
+            NomM = input()
+            VidM = Dado.tirav()
+            AtaM = Dado.tiraa()
+            DefM = Dado.tirad()
+            Momia1 = Momia(NomM, VidM, AtaM, DefM)
+            
+            print("\n\tVamos a definir el nombre del BARBARO")  
+            print("Nombre Barbaro")
+            NomB = input()
+            VidB = Dado.tirav()
+            AtaB = Dado.tiraa()
+            DefB = Dado.tirad()
+            Barbaro1 = Barbaro(NomB, VidB, AtaB, DefB)
+        #Si es manual, el usuario por medio de imputs() selecciona los datos
+        elif opcion == "2":    
+            print("\tVamos a definir los valores de la MOMIA") 
+            print("Nombre Momia")
+            NomM = input()
+            print("Vida Momia")
+            VidM = int(input())
+            print("Ataque Momia")
+            AtaM = int(input())
+            print("Defensa Momia")
+            DefM = int(input())
+            Momia1 = Momia(NomM, VidM, AtaM, DefM)
+
+            print("\n\tVamos a definir los valores del BARBARO")  
+            print("Nombre Barbaro")
+            NomB = input()
+            print("Vida Barbaro")
+            VidB = int(input())
+            print("Ataque Barbaro")
+            AtaB = int(input())
+            print("Defensa Barbaro")
+            DefB = int(input())
+            Barbaro1 = Barbaro(NomB, VidB, AtaB, DefB)
+        else:
+            print("¡ERROR!\nSelecciona una opcion valida")
+            
+        #Se borran los textos
+        clear_output()
+
+        #Se muestran los parametros de cada personaje y las opciones de la partida y se guardan en los logs
+        if opcion == "1":
+            print("Módo de elección de parámetros: Automático")
+        elif opcion == "2":
+            print("Módo de elección de parámetros: Manual")
+        print("\n", Momia1)
+        print("\n", Barbaro1)
+        LogPersonajes(Momia1.nombre, Momia1.logInfo(), Barbaro1.nombre, Barbaro1.logInfo())
+
+        #Se indican los turnos
+        print("\n¿Cuántos turnos máximos quiere jugar?: ")
+        turnos = int(input())
+        #Se asigna los valores para el sistema de turnos. J1 es la momia y J2 es el barbaro.
         Atacante = "Bárbaro"
-    if Defensor == "Bárbaro":
         Defensor = "Momia"
-    elif Defensor == "Momia":
-        Defensor = "Bárbaro"
-    turnolog = ("\n<<< TURNO {} | {} ataca a {} >>>".format(x + 1, Atacante, Defensor))
-    print(turnolog)
-    #Dependiendo de quien es el Jugador 1 (atacante) se ejecutara el .atacar de un jugador u otro
-    if Atacante == "Bárbaro":
-        Numataques = Barbaro1.atacar()
-        Momia1.defender(Numataques)
-    elif Atacante == "Momia":
-        Numataques = Momia1.atacar()
-        Barbaro1.defender(Numataques)
-    #tras los ataques se revisa si los jugadores estan vivos
-    Barbaro1.estarVivo()
-    Momia1.estarVivo()
-    #Dependiendo de lo que devuelva .personajeVivo() se sigue o no con el combate
-    if Barbaro1.personajeVivo == False:
-        resultado = ("\n\tEl ganador del combate es la Momia {} con {} pt de vida".format(Momia1.nombre, Momia1.vida))
-        break
-    if Momia1.personajeVivo == False:
-        resultado = ("\n\tEl ganador del combate es el Barbaro {} con {} pt de vida".format(Barbaro1.nombre, Barbaro1.vida))
-        break
-if Barbaro1.personajeVivo == True and Momia1.personajeVivo == True:
-    resultado = ("\n\tEl combate queda en un empate! Ambos jugadores sobreviven!")
-print(resultado)
+        turno = 0
+        for x in range(turnos):
+            turno += 1
+            #Mediante un bucle y varios If deternminamos los turnos
+            time.sleep(1) #implentamos un timesleep(x) para poder hacer que los bucles tengan pausa de x segundos.
+            if Atacante == "Bárbaro":
+                Atacante = "Momia"
+            elif Atacante == "Momia":
+                Atacante = "Bárbaro"
+            if Defensor == "Bárbaro":
+                Defensor = "Momia"
+            elif Defensor == "Momia":
+                Defensor = "Bárbaro"
+            turnolog = ("\n<<< TURNO {} | {} ataca a {} >>>".format(x + 1, Atacante, Defensor))
+            print(turnolog)
+            #Dependiendo de quien es el Jugador 1 (atacante) se ejecutara el .atacar de un jugador u otro
+            if Atacante == "Bárbaro":
+                Numataques = Barbaro1.atacar()
+                Momia1.defender(Numataques)
+            elif Atacante == "Momia":
+                Numataques = Momia1.atacar()
+                Barbaro1.defender(Numataques)
+            #tras los ataques se revisa si los jugadores estan vivos
+            Barbaro1.estarVivo()
+            Momia1.estarVivo()
+            #Dependiendo de lo que devuelva .personajeVivo() se sigue o no con el combate
+            if Barbaro1.personajeVivo == False:
+                resultado = ("\n\tEl ganador del combate es la Momia {} con {} pt de vida".format(Momia1.nombre, Momia1.vida))
+                break
+            if Momia1.personajeVivo == False:
+                resultado = ("\n\tEl ganador del combate es el Barbaro {} con {} pt de vida".format(Barbaro1.nombre, Barbaro1.vida))
+                break
+        if Barbaro1.personajeVivo == True and Momia1.personajeVivo == True:
+            resultado = ("\n\tEl combate queda en un empate! Ambos jugadores sobreviven!")
+        print(resultado)
 
-#Se muestra por pantalla los resultados y se guardan los logs de la partida.        
-ResultadoMomia = ("\n\tMOMIA:   {} queda con {} pt de vida.".format(Momia1.nombre, Momia1.vida))    
-ResultadoBarbaro = ("\n\tBARBARO: {} queda con {} pt de vida.".format(Barbaro1.nombre, Barbaro1.vida))
-ResultadoPartida = (ResultadoMomia + ResultadoBarbaro)
-print(ResultadoPartida)
-LogPartida(Momia1.logInfo(),Momia1.nombre, Barbaro1.logInfo(),Barbaro1.nombre, opcion, turno, turnos, ResultadoPartida)
+        #Se muestra por pantalla los resultados y se guardan los logs de la partida.        
+        ResultadoMomia = ("\n\tMOMIA:   {} queda con {} pt de vida.".format(Momia1.nombre, Momia1.vida))    
+        ResultadoBarbaro = ("\n\tBARBARO: {} queda con {} pt de vida.".format(Barbaro1.nombre, Barbaro1.vida))
+        ResultadoPartida = (ResultadoMomia + ResultadoBarbaro)
+        print(ResultadoPartida)
+        LogPartida(Momia1.logInfo(),Momia1.nombre, Barbaro1.logInfo(),Barbaro1.nombre, opcion, turno, turnos, ResultadoPartida)
+        gamestatus = "end"
+    else:
+        print("\n-- Volver a jugar: 1\t|\tVolver al menu: 2\t|\tSalir: 3 --\n")
+        GameOver = input()
+        while True:
+            if GameOver == "1":
+                gamestatus = 2
+                break
+            if GameOver == "2":
+                gamestatus = 1
+                break
+            if GameOver== "3":
+                quit()
+                break
 
 
 #Comming soon English version!
