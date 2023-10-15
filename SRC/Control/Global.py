b37e314c25ee3b11dc6dfffa8b9950616eed26fa
HY_ElCositas <61937572+VanimacionYT@users.noreply.github.com>
2023-10-15 12:32:02 +0200

    Added Dice Default Values in Global

--- SRC/Control/Global.py
+++ SRC/Control/Global.py
@@ -2,3 +2,7 @@ from SRC.Control.Current import CurrentState
 
 class GlobalState():
     GlobalGame = CurrentState.TITLE
+    GlobalDiceFaces = 6
+    HealthDiceFaces = 15
+    AttackDiceFaces = 10
+    DefenseDiceFaces = 10
