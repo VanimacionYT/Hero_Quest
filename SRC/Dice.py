8b7b153906f899a56ce1f8e2d491802cb798db41
HY_ElCositas <61937572+VanimacionYT@users.noreply.github.com>
2023-10-25 19:18:49 +0200

    WIP Dice Value System Making all Dice values able to change by the user in the future using a value log file an modifying it inside the app. Added the capability to modify the dice values in a file.

--- SRC/Dice.py
+++ SRC/Dice.py
@@ -1,41 +1,49 @@
 import random
 from IPython.display import clear_output
 
+GlobalDiceMinFaces = 0
+GlobalDiceMaxFaces = 0
+HealthDiceMinFaces = 0
+HealthDiceMaxFaces = 0
+AttackDiceMinFaces = 0
+AttackDiceMaxFaces = 0
+DefenseDiceMinFaces = 0
+DefenseDiceMaxFaces = 0
 
 def DiceInitiator():
     with open('Log/LogDices.txt', 'r') as DiceLog:
         DiceIndividualSettings = DiceLog.readline().split(".")
-        print(DiceIndividualSettings)
-        GlobalDiceMinFaces = DiceIndividualSettings[0].split("-")[0]
-        GlobalDiceMaxFaces = DiceIndividualSettings[0].split("-")[1]
-        HealthDiceMinFaces = DiceIndividualSettings[1].split("-")[0]
-        HealthDiceMaxFaces = DiceIndividualSettings[1].split("-")[1]
-        AttackDiceMinFaces = DiceIndividualSettings[2].split("-")[0]
-        AttackDiceMaxFaces = DiceIndividualSettings[2].split("-")[1]
-        DefenseDiceMinFaces = DiceIndividualSettings[3].split("-")[0]
-        DefenseDiceMaxFaces = DiceIndividualSettings[3].split("-")[1]
-    return GlobalDiceMinFaces,GlobalDiceMaxFaces,HealthDiceMinFaces,HealthDiceMaxFaces,AttackDiceMinFaces,AttackDiceMaxFaces,DefenseDiceMinFaces,DefenseDiceMaxFaces
-        
+        DIGlobalDiceMinFaces = int(DiceIndividualSettings[0].split("-")[0])
+        DIGlobalDiceMaxFaces = int(DiceIndividualSettings[0].split("-")[1])
+        DIHealthDiceMinFaces = int(DiceIndividualSettings[1].split("-")[0])
+        DIHealthDiceMaxFaces = int(DiceIndividualSettings[1].split("-")[1])
+        DIAttackDiceMinFaces = int(DiceIndividualSettings[2].split("-")[0])
+        DIAttackDiceMaxFaces = int(DiceIndividualSettings[2].split("-")[1])
+        DIDefenseDiceMinFaces = int(DiceIndividualSettings[3].split("-")[0])
+        DIDefenseDiceMaxFaces = int(DiceIndividualSettings[3].split("-")[1])
+        return DIGlobalDiceMinFaces,DIGlobalDiceMaxFaces,DIHealthDiceMinFaces,DIHealthDiceMaxFaces,DIAttackDiceMinFaces,DIAttackDiceMaxFaces,DIDefenseDiceMinFaces,DIDefenseDiceMaxFaces
+    
+    
 
 class Dice():
     def GlobalDice():
         #DiceFaces = 6
-        Dice = (random.randint(GlobalDiceMinFaces, GlobalDiceMaxFaces))
+        Dice = (random.randint(DiceInitiator()[0], DiceInitiator()[1]))
         return Dice
     
     def DiceHealth():
         #DiceFaces = 15
-        Dice = (random.randint(HealthDiceMinFaces, HealthDiceMaxFaces))
+        Dice = (random.randint(DiceInitiator()[2], DiceInitiator()[3]))
         return Dice
     
     def DiceAttack():
         #DiceFaces = 10
-        Dice = (random.randint(AttackDiceMinFaces, AttackDiceMaxFaces))
+        Dice = (random.randint(DiceInitiator()[4], DiceInitiator()[5]))
         return Dice
     
     def DiceDefense():
         #DiceFaces = 10
-        Dice = (random.randint(DefenseDiceMinFaces, DefenseDiceMaxFaces))
+        Dice = (random.randint(DiceInitiator()[6], DiceInitiator()[7]))
         return Dice
     
     def FirstPlayerTurn():
---
+++ SRC/TestDice.py
@@ -0,0 +1,4 @@
+from Dice import Dice, DiceInitiator
+
+numero = DiceInitiator()[0]
+print(numero)
